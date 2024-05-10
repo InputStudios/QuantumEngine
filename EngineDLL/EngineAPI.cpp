c6f218e9695067ad0542a9fccfd40d12dca1c221
Андрей Трепалин <Hitmanki@yandex.ru>
2024-05-10 16:48:53 +0400

    added a new project EngineDLL

---
+++ EngineDLL/EngineAPI.cpp
@@ -0,0 +1,57 @@
+// Copyright (c) Andrey Trepalin. 
+// Distributed under the MIT license. See the LICENSE file in the project root for more information.
+
+#include "Common.h"
+#include "CommonHeaders.h"
+#include "..\Engine\Components\Script.h"
+
+#ifndef WIN32_MEAN_AND_LEAN
+#define WIN32_MEAN_AND_LEAN
+#endif
+
+#include <Windows.h>
+
+using namespace Quantum;
+
+namespace {
+    HMODULE game_code_dll{ nullptr };
+    using _get_script_creator = Quantum::script::detail::script_creator(*)(size_t);
+    _get_script_creator get_script_creator{ nullptr };
+    using _get_script_names = LPSAFEARRAY(*)(void);
+    _get_script_names get_script_names{ nullptr };
+} // anonymou namespace
+
+EDITOR_INTERFACE u32
+LoadGameCodeDll(const char* dll_path)
+{
+    if (game_code_dll) return FALSE;
+    game_code_dll = LoadLibraryA(dll_path);
+    assert(game_code_dll);
+
+    get_script_creator = (_get_script_creator)GetProcAddress(game_code_dll, "get_script_creator");
+    get_script_names = (_get_script_names)GetProcAddress(game_code_dll, "get_script_names");
+
+    return (game_code_dll && get_script_creator && get_script_names) ? TRUE : FALSE;
+}
+
+EDITOR_INTERFACE u32
+UnloadGameCodeDll(const char* dll_path)
+{
+    if (game_code_dll) return FALSE;
+    assert(game_code_dll);
+    int result{ FreeLibrary(game_code_dll) };
+    assert(result);
+    game_code_dll = nullptr;
+    return TRUE;
+}
+
+EDITOR_INTERFACE script::detail::script_creator
+GetScriptCreator(const char* name)
+{
+    return (game_code_dll && get_script_creator) ? get_script_creator(script::detail::string_hash()(name)) : nullptr;
+}
+
+EDITOR_INTERFACE LPSAFEARRAY GetScriptNames()
+{
+    return (game_code_dll && get_script_creator) ? get_script_names() : nullptr;
+}
