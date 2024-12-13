697fe50936d41b1338b1b1b1f9cb02de88304027
Андрей Трепалин <Hitmanki@yandex.ru>
2024-12-13 21:56:27 +0400

    Updated shader loading compilation

--- EngineTest/ShaderCompilation.h
+++ EngineTest/ShaderCompilation.h
@@ -2,5 +2,29 @@
 // Distributed under the MIT license. See the LICENSE file in the project root for more information.
 
 #pragma once
+#include "CommonHeaders.h"
 
+struct shader_type {
+    enum type : u32 {
+        vertex = 0,
+        hull,
+        domain,
+        geometry,
+        pixel,
+        compute,
+        amplification,
+        mesh,
+
+        count
+    };
+};
+
+struct shader_file_info
+{
+    const char*             file_name;
+    const char*             function;
+    shader_type::type       type;
+};
+
+std::unique_ptr<u8[]> compile_shader(shader_file_info info, const char* file_path, Quantum::util::vector<std::wstring>& extra_args);
 bool compile_shaders();
